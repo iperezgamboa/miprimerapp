3c2975b9d864843ae2f5260968e9045f0557657d
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2021-01-07 16:32:37 -0300

    segundocommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/one'
+  get 'pages/two'
+  
+  #define pagina one como de inicio.
+  root 'pages#one'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
